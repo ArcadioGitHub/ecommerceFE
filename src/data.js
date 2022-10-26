372962f912b7f03e32c9e7450e2517b05fc254c5
Arcadio Buelvas <arcadiobuelvas>
2022-10-26 13:15:17 -0500

    FE version 1.0

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/5F504WG/ImageTwo.jpg",
+        title: "BASIC COLLECTION",
+        desc: "IF YOU WANT TO LOOK AS COOL AS I DO, GO AND TELL YOUR HUMAN TO BUY YOU SOME CLOTHES AT MR. TEO APPAREL",
+        bg: "fdf5e6",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/wY7wYCM/Teo.jpg",
+        title: "FRESH COLLECTION",
+        desc: "IF YOU WANT TO LOOK AS COOL AS I DO, GO AND TELL YOUR HUMAN TO BUY YOU SOME CLOTHES AT MR. TEO APPAREL",
+        bg: "BCD8E3",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/rkZRMpb/Fancy.jpg",
+        title: "FANCY COLLECTION",
+        desc: "IF YOU WANT TO LOOK AS COOL AS I DO, GO AND TELL YOUR HUMAN TO BUY YOU SOME CLOTHES AT MR. TEO APPAREL",
+        bg: "DFF6CA",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://s.alicdn.com/@sc04/kf/Hf201da8584d14bc89283623bb811311cs.jpg_960x960.jpg",
+        title: "BASIC COLLECTION!",
+    },
+    {
+        id: 2,
+        img: "https://s.alicdn.com/@sc04/kf/H83f514f0aa2641cbadeb88b46807e14c7.jpg_960x960.jpg",
+        title: "FRESH COLLECTION",
+    },
+    {
+        id: 3,
+        img: "https://s.alicdn.com/@sc04/kf/H149d06f3c70f4f02ad9c5cfe8e21163fQ.jpg_960x960.jpg",
+        title: "FANCY COLLECTION",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://s.alicdn.com/@sc04/kf/H3f149d36cc1149f494ed95e72def5196N.jpg_960x960.jpg",
+    },
+    {
+        id: 2,
+        img: "https://s.alicdn.com/@sc04/kf/H8ba7f4e46b5b4a259b8df743e7fab279T.jpg_960x960.jpg",
+    },
+    {
+        id: 3,
+        img: "https://s.alicdn.com/@sc04/kf/Hcecc98cc43ec44d48929facc3d31230bx.jpg_960x960.jpg",
+    },
+    {
+        id: 4,
+        img: "https://s.alicdn.com/@sc04/kf/He1b11ac3de5e41a0af647e09e091390cs.jpg_960x960.jpg",
+    },
+    {
+        id: 5,
+        img: "https://s.alicdn.com/@sc04/kf/Hbb532a5cf0c6460093bfcc5a91949f42E.jpg_960x960.jpg",
+    },
+    {
+        id: 6,
+        img: "https://s.alicdn.com/@sc04/kf/H60b113074a4b48099c49533655b328c5z.jpg_960x960.jpg",
+    },
+    {
+        id: 7,
+        img: "https://s.alicdn.com/@sc04/kf/H751e095e4bf943b78cae90faa6390c63c.jpg_960x960.jpg",
+    },
+    {
+        id: 8,
+        img: "https://s.alicdn.com/@sc04/kf/H3386fccc7027419a80d0d6b10e02ea1b7.jpg_960x960.jpg",
+    },
+    
+]
